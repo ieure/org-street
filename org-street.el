431d887a1fc7bf69bea27cbd561a8a4c1ce2f826
Ian Eure <ian@retrospec.tv>
2019-07-09 23:19:34 -0700

    Refactor into separate libs

--- org-street.el
+++ org-street.el
@@ -1,55 +1,9 @@
 
-(require 'dbus)
-(require 's)
-
-(defconst org-street--nominatim-url "https://nominatim.openstreetmap.org"
-  "Base URL of Nominatim.")
-
-(defun org-street-query (text)
-  "Look up TEXT with Nominatim.
-   Returns an array of results."
-  (with-current-buffer
-      (thread-first
-          (format "%s/search?format=json&addressdetails=1&q=%s"
-                  org-street--nominatim-url text)
-        (url-retrieve-synchronously t))
-    (goto-char (point-min))
-    (search-forward "\n\n")
-    (json-read)))
-
-(defun org-street--current-location ()
-  
-  )
-
-(defun org-street--fields->str (entry fields)
-  "Return a printable version of fields FIELDS in ENTRY."
-  (let* ((type (assoc 'type entry))
-         (entry-type-key (intern (cdr type)))
-         (address (cons type
-                        (cdr (assoc 'address entry))))
-         (address (cons
-                   `(type-value . ,(cdr (assoc entry-type-key address)))
-                   address))
-         (commas '(type-value road city)))
-    (thread-last
-        (cl-loop for field in fields
-                 for raw-val = (cdr (assoc field address))
-                 for val = (if (memq field commas)
-                               (concat raw-val ",")
-                             raw-val)
-                 when val collect val)
-      (s-join " "))))
-
-(defun org-street--address (entry)
-  "Return a printable address for a Nominatim result."
-  (org-street--fields->str
-   entry
-   '(type-value house_number road city state postcode country)))
+(require 'nominatim)
 
 (defun org-street-org-set-location (place)
   (interactive (list (read-string "Location: ")))
-
-  (let* ((matches (org-street-query place))
+  (let* ((matches (nominatim-geocode place))
          (n (length matches)))
     (cond
      ;; No matches, error
@@ -59,25 +13,25 @@
      ((= n 1) (org-set-property "LOCATION" (org-street--address (aref matches 0))))
 
      ;; Multiple matches, prompt
-     (t ))))
+     (t (error "Not yet implemented.")))))
 
 (defun org-street-next-location (&optional arg)
-  (interactive "P"))
+  (interactive "P")
+  (goto-char (field-end))
+  (forward-line 1))
 
 (defun org-street-previous-location (&optional arg)
-  (interactive "P"))
-
-(defun org-street-choose-location ()
-  (interactive))
+  (interactive "P")
+  (goto-char (field-beginning))
+  (forward-line -1))
 
 (defvar org-street-choose-mode-map
   (let ((kmap (make-sparse-keymap)))
     (define-key kmap "q" 'kill-buffer-and-window)
-    (define-key kmap "n" 'next-line)
-    (define-key kmap "p" 'previous-line)
-    (define-key kmap "M-n" 'org-street-next-location)
-    (define-key kmap "M-P" 'org-street-previous-location)
+    (define-key kmap "n" 'org-street-next-location)
+    (define-key kmap "p" 'org-street-previous-location)
     (define-key kmap (kbd "RET") 'org-street-choose-location)
+    (define-key kmap "mouse-1" 'org-street-choose-location)
     kmap))
 
 (define-derived-mode org-street-choose-mode fundamental-mode "Org Street"
@@ -85,17 +39,32 @@
   (setq buffer-read-only t
         buffer-modified-p nil))
 
+(defconst org-street-choose-mode-font-lock-keywords
+  `(
+    (,(rx bol
+          (group (minimal-match (1+ anything)))
+          "(" (group (1+ anything)) ")" eol)
+     (1 bold)
+     (2 hi-yellow)))
+
+  "Font-locking defintions for ‘org-street-choose-mode’.")
+
 (defun org-street--insert-locations (locations)
   (cl-loop for loc across locations
+           for address = (cdr (assoc 'address loc))
            for type = (intern (cdr (assoc 'type loc)))
            do
-           (insert (format "%s (%s)\n" (cdr (assoc type loc)) type))
-           (insert (concat "  " (org-street--fields->str loc '(house_number road))
-                           "\n"))
-           (insert (concat "  " (org-street--fields->str loc '(city state portcode))
-                           "\n"))
-           (insert (concat "  " (org-street--fields->str loc '(country))
-                           "\n\n"))))
+           (insert
+            (thread-first
+                (concat
+                 (propertize (cdr (assoc type address)) 'face 'bold)
+                 " (" (propertize (cdr (assoc 'type loc)) 'face 'hi-yellow) ")\n"
+
+                 "  " (org-street--fields->str loc '(house_number road)) "\n"
+                 "  " (org-street--fields->str loc '(city state portcode)) "\n"
+                 "  " (org-street--fields->str loc '(country)) "\n")
+              (propertize 'field t 'mouse-face 'highlight)
+              (concat "\n")))))
 
 (defun org-street--choose (search-text locations)
   (with-current-buffer (get-buffer-create (format "*Org Street[%s]*" search-text))
@@ -104,6 +73,7 @@
     (erase-buffer)
     (org-street--insert-locations locations)
     (org-street-choose-mode)
+    (goto-char (point-min))
     (pop-to-buffer (current-buffer))))
 
 (provide 'org-street)
