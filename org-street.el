5e5044b06361ffe0bb644607a3657550feb2e42e
Ian Eure <ian@retrospec.tv>
2019-07-20 08:20:41 -0700

    Factor out printable address stuff, delete os-ng

--- org-street.el
+++ org-street.el
@@ -1,92 +1,16 @@
+;;; org-street.el --- sdfsodifj
+
+
+;;; Commentary:
+;; 
 
 (require 'nominatim)
+
+;;; Code:
 
 (defvar org-street--target nil
   "The target buffer and position to set the chosen location.")
 (make-variable-buffer-local 'org-street--target)
-
-(defun org-street-org-set-location (place)
-  "Set the LOCATION property of this Org entry."
-  (interactive (list (read-string "Location: ")))
-  (let* ((matches (nominatim-geocode place))
-         (n (length matches)))
-    (cond
-     ;; No matches, error
-     ((= n 0) (error "No matches for `%s'" place))
-
-     ;; One match, put it in.
-     ((= n 1) (org-set-property "LOCATION" (org-street--address (aref matches 0))))
-
-     ;; Multiple matches, prompt
-     (t (org-street--choose place (cons (current-buffer) (point)) matches)))))
-
-(defun org-street--printable (loc)
-  (let ((cc (thread-last (assoc 'address loc)
-              (cdr)
-              (assoc 'country_code)
-              (cdr))))
-    (cond ((string= cc "us") (org-street--printable-us loc))
-          (t (error "Don't know how to handle addresses in `%s'" cc)))))
-
-(defun org-street--printable-us (loc)
-  (let ((elements)
-        (addr (cdr (assoc 'address loc)))
-        (type-sym (intern (cdr (assoc 'type loc)))))
-
-    ;; Business name
-    (if-let ((biz-name (cdr (assoc type-sym addr))))
-        (progn
-          (push biz-name elements)
-          (push :break elements)))
-
-    ;; House number
-    (if-let ((house-number (cdr (assoc 'house_number addr))))
-        (push house-number elements))
-
-    ;; Road
-    (if-let ((road (cdr (assoc 'road addr))))
-        (progn
-          (push road elements)
-          (push :break elements)))
-
-    ;; City
-    (if-let ((city-or-county (or (cdr (assoc 'city addr))
-                                 (cdr (assoc 'county addr)))))
-        (progn
-          (push city-or-county elements)
-          (push :soft-break elements)))
-
-    ;; State
-    (if-let ((state (cdr (assoc 'state addr))))
-        (progn
-          (push state elements)))
-
-    ;; Postcode
-    (if-let ((postcode (cdr (assoc 'postcode addr))))
-        (progn
-          (push postcode elements)
-          (push :break elements)))
-
-    ;; Country
-    (if-let ((country (cdr (assoc 'country addr))))
-        (push country elements))
-
-    (reverse elements)))
-
-(defun org-street--printable->oneline (printable-loc)
-  (thread-first
-      (lambda (s elt)
-        (cond ((keywordp elt) (concat s ","))
-              (t (concat s " " elt))))
-    (reduce printable-loc)))
-
-(defun org-street--printable->nline (printable-loc)
-  (thread-first
-      (lambda (s elt)
-        (cond ((eq :soft-break elt) (concat s ","))
-              ((eq :break elt) (concat s "\n"))
-              (t (concat s " " elt))))
-    (reduce printable-loc)))
 
 (defun org-street-choose-select-location ()
   "Choose a location."
@@ -112,22 +36,30 @@
     (define-key kmap (kbd "<up>") 'org-previous-visible-heading)
     (define-key kmap "q" 'kill-buffer-and-window)
     (define-key kmap (kbd "RET") 'org-street-choose-select-location)
-    kmap))
+    kmap)
+  "Keymap for `org-street-choose' major mode.")
 
 (define-derived-mode org-street-choose-mode org-mode
   "Org Street Choose"
   "Major mode to choose an Org Street result.")
 
 (defun org-street--insert-locations (locations)
+  "Place LOCATIONS into the `org-street-choose' buffer."
   (cl-loop for loc across locations
            do
            (insert (format "* %s :%s:\n"
                            (thread-first loc
-                             (org-street--printable)
-                             (org-street--printable->oneline))
+                             (nominatum--printable)
+                             (nominatum--printable->oneline))
                            (cdr (assoc 'type loc))))))
 
 (defun org-street--choose (search-text target locations)
+  "Bring up the org-street choose buffer.
+
+   SEARCH-TEXT is the text that was used for the nominatim
+   search.  TARGET is a cons cell of (BUFFER . LOCATION), the
+   place the LOCATION property should be set.  LOCATIONS is the
+   list of possible choices."
   (with-current-buffer (get-buffer-create (format "*Org Street[%s]*" search-text))
     (widen)
     (setq buffer-read-only nil)
@@ -135,10 +67,35 @@
     (org-street-choose-mode)
     (org-street--insert-locations locations)
     (indent-region (point-min) (point-max))
-    (org-street-choose-mode)
-    (setq buffer-read-only t)
-    (setq org-street--target target)
+    (setq buffer-read-only t
+          org-street--target target)
     (goto-char (point-min))
     (pop-to-buffer (current-buffer))))
 
+ ;; User-serviceable parts
+
+(defun org-street-org-set-location (text)
+  "Set the LOCATION property of the current entry to the address of TEXT.
+
+   Nominatum is used to look up TEXT.  If a single location is
+   returned, its address is used for the LOCATION property.  If
+   multiple locations are returned, a list is displayed to choose
+   from."
+  (interactive (list (read-string "Location: ")))
+  (let* ((matches (nominatim-geocode place))
+         (n (length matches)))
+    (cond
+     ;; No matches, error
+     ((= n 0) (error "No matches for `%s'" place))
+
+     ;; One match, put it in.
+     ((= n 1) (org-set-property "LOCATION" (org-street--address (aref matches 0))))
+
+     ;; Multiple matches, prompt
+     (t (org-street--choose place (cons (current-buffer) (point)) matches)))))
+
 (provide 'org-street)
+
+(provide 'org-street)
+
+;;; org-street.el ends here
